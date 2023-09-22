632fd3d755e05c60bf4549a9518eafadf87dee76
hgzech <hilmarzech@gmail.com>
2023-09-22 13:05:11 +0300

    Added exchange interface and stubs for filtering functions.

--- email_filter/_modidx.py
+++ email_filter/_modidx.py
@@ -5,4 +5,11 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://hgzech.github.io',
                 'git_url': 'https://github.com/hgzech/email_filter',
                 'lib_path': 'email_filter'},
-  'syms': {'email_filter.core': {'email_filter.core.foo': ('core.html#foo', 'email_filter/core.py')}}}
+  'syms': { 'email_filter.domain_age': { 'email_filter.domain_age.check_domain_age': ( 'domain_age.html#check_domain_age',
+                                                                                       'email_filter/domain_age.py'),
+                                         'email_filter.domain_age.parse_domain': ( 'domain_age.html#parse_domain',
+                                                                                   'email_filter/domain_age.py')},
+            'email_filter.junk_checker': { 'email_filter.junk_checker.check_domain_age': ( 'junk_checker.html#check_domain_age',
+                                                                                           'email_filter/junk_checker.py'),
+                                           'email_filter.junk_checker.parse_domain': ( 'junk_checker.html#parse_domain',
+                                                                                       'email_filter/junk_checker.py')}}}
